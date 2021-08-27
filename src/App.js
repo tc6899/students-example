c8cccd53be574b35dc27d9f207cd69113a7320e5
David Y. Stephenson <david@davidystephenson.com>
2021-08-27 17:45:21 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,9 @@
+export default function App () {
+  return (
+    <main>
+      <h1>Hello</h1>
+
+      <p>world</p>
+    </main>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const app = (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+const root = document.getElementById('root')
+
+ReactDOM.render(app, root)
